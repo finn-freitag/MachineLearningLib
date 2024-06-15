407f2b5b7ab3d51f8459944b68d97305582954d2
Finn Freitag <finn-freitag@web.de>
2024-06-15 12:04:53 +0200

    feat: LeakyReLU

---
+++ MachineLearningLib/ActivationFunctions/LeakyReLU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearningLib.ActivationFunctions
+{
+    public class LeakyReLU : IActivationFunction
+    {
+        float alpha = 0.1f;
+
+        public LeakyReLU(float alpha = 0.01f)
+        {
+            this.alpha = alpha;
+        }
+
+        public float Derivative(float x)
+        {
+            if (x > 0)
+                return 1;
+            else
+                return alpha;
+        }
+
+        public float Evaluate(float x)
+        {
+            if (x > 0)
+                return x;
+            else
+                return alpha * x;
+        }
+    }
+}
