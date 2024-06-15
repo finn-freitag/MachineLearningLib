8997bdc141f4abb138e304a01338c20ef35f3110
Finn Freitag <finn-freitag@web.de>
2024-06-15 12:07:56 +0200

    feat: Linear activation function

---
+++ MachineLearningLib/ActivationFunctions/Linear.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineLearningLib.ActivationFunctions
+{
+    public class Linear : IActivationFunction
+    {
+        public float Derivative(float x)
+        {
+            return 1;
+        }
+
+        public float Evaluate(float x)
+        {
+            return x;
+        }
+    }
+}
